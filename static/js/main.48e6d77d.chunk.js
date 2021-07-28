7323066a449b4875229fd6052bcbc680bdc25143
Michelle Nguyen <michellenguyen11239@aol.com>
2021-07-27 17:16:01 -0700

    Updates

---
+++ static/js/main.48e6d77d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-portfolio1"]=this["webpackJsonpreact-portfolio1"]||[]).push([[0],{10:function(t,e,c){"use strict";c.r(e);var n=c(1),r=c.n(n),i=c(3),o=c.n(i),s=(c(8),c(0));var a=function(){return Object(s.jsx)("div",{children:Object(s.jsx)("h1",{children:"Initial commit"})})};o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.48e6d77d.chunk.js.map
