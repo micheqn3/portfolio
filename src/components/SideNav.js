94280776bc0446db23891437df9cefc14cc041ae
Michelle Nguyen <michellenguyen11239@aol.com>
2021-08-05 12:55:15 -0700

    Added media query for smaller devices and spaced out side nav items

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -5,7 +5,7 @@ import React from 'react';
 const SideNav = ({handlePageChange}) => {
     return (
         <ul className="sidenav" id="mobile-demo">
-            <li><a onClick={() => handlePageChange('main')} href="index.html" target="_self">About Me</a></li>
+            <li className="firstnav"><a onClick={() => handlePageChange('main')} href="index.html" target="_self">About Me</a></li>
             <li><a onClick={() => handlePageChange('portfolio')} href="#portfolio-container" target="_self" >Portfolio</a></li>
         </ul>
     )
