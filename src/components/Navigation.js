aad625612794fd7ce115c131a6543fcedba1f0d8
Michelle Nguyen <michellenguyen11239@aol.com>
2021-09-09 16:14:03 -0700

    Updated app by removing active tab and moving all pages to main

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,20 +3,19 @@
 import React from 'react';
 import ContactModal from './ContactModal';
 
-const Navigation = ({page, handlePageChange}) => {
+const Navigation = () => {
     return (
         <nav> 
             <div className="nav-wrapper">
             <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons text-darken-4 cyan-text">menu</i></a>
 				<ul className="hide-on-med-and-down">
-					<li><a onClick={() => handlePageChange('main')} className={page === 'main' ? 'nav-tab active-nav' : 'nav-tab'} href="index.html">About Me</a></li>
-					<li><a onClick={() => handlePageChange('portfolio')} className={page === 'portfolio' ? 'nav-tab active-nav' : 'nav-tab'} href="#portfolio-container">Portfolio</a></li>
+					<li><a className="nav-tab" href="index.html">About Me</a></li>
+					<li><a className="nav-tab" href="#portfolio-container">Portfolio</a></li>
 					<li><a className="contact-btn my-btn waves-effect waves-light btn-small modal-trigger" href="#modal2">Contact Me</a></li>
 				</ul>
             </div>
             <ContactModal/>
         </nav>
-
     )
 }
 
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,7 +1,7 @@
 /* Portfolio container */
 
 // Import all react dependencies and components
-import React, {useState} from 'react';
+import React from 'react';
 import Header from './Header';
 import Main from './pages/Main';
 import Portfolio from './pages/Portfolio';
@@ -9,30 +9,12 @@ import Footer from './Footer';
 
 const PortfolioContainer = () => {
 
-    // Default page is main page
-    const [page, setPage] = useState('main');
-
-    // Renders page based on change of page state
-    const renderPage = () => {
-        if (page === 'main') {
-            return <Main/>;
-        } else if (page === 'portfolio') {
-            return <Portfolio/>;
-        } else {
-            console.log('There was an error in rendering');
-        }
-    }
-
-    // Changes page state on click
-    const handlePageChange = (page) => {
-        setPage(page)
-    }
-
     return (
         <div>
             <div className="wrapper">
-                <Header page={page} handlePageChange={handlePageChange}/>
-                {renderPage()}
+                <Header/>
+                <Main/>
+                <Portfolio/>
                 <Footer/>
             </div>
         </div>
